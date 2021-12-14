b87f3aa273a7e6586160cb23e61bc0fb1dd97f62
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2021-12-14 10:20:58 +0100

    Update black color and font

--- config.h
+++ config.h
@@ -1,11 +1,10 @@
-static int topbar = 1;   
-static const char *fonts[] = { "Source Code Pro:size=16" };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static int topbar = 1;
+static const char *fonts[] = { "Source Code Pro:pixelsize=16:antialias=true:autohint=true" };
+static const char *prompt = NULL;
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#383a42", "#fafafa" },
-	[SchemeSel] = { "#383a42", "#50a14f" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+  [SchemeNorm] = { "#000000", "#fafafa" },
+  [SchemeSel] = { "#000000", "#50a14f" },
+  [SchemeOut] = { "#000000", "#00ffff" },
 };
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 static const char worddelimiters[] = " ";
