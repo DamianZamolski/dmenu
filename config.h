ccad22090b44a7f1e115b121f9c6b72a1795eed0
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2022-11-06 18:27:10 +0100

    Match font size to dwm

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 static int topbar = 1;
-static const char *fonts[] = { "SauceCodePro Nerd Font Mono:pixelsize=16:antialias=true:autohint=true" };
-static const char *prompt = NULL;
-static const char *colors[SchemeLast][2] = {
+static const char* fonts[] = { "SauceCodePro Nerd Font Mono:pixelsize=24:antialias=true:autohint=true" };
+static const char* prompt = NULL;
+static const char* colors[SchemeLast][2] = {
   [SchemeNorm] = { "#000000", "#fafafa" },
   [SchemeSel] = { "#000000", "#50a14f" },
   [SchemeOut] = { "#000000", "#00ffff" },
